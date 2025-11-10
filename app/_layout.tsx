7228f301413a298aa4ef4aef26d953410de6a2a0
Miroir Raphaël <miroir.raphael07@gmail.com>
2025-11-10 12:28:20 +0100

    test commitlint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,18 +5,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
 
 export default function RootLayout() {
-  const isAuthenticated = false; // ta vraie logique ici
+  const isAuthenticated = false;
   const segments = useSegments();
 
-  // Vérifie si on est dans le groupe auth
   const inAuthGroup = segments[0] === "(auth)";
 
-  // Si pas authentifié et pas dans le groupe auth → redirige vers sign-up
   if (!isAuthenticated && !inAuthGroup) {
     return <Redirect href="/(auth)/sign-up" />;
   }
 
-  // Si authentifié et on est dans le groupe auth → renvoie vers les tabs
   if (isAuthenticated && inAuthGroup) {
     return <Redirect href="/(tabs)" />;
   }
